d13d87df8b3dfc930e25a31c4491c71a5639b4ff
nazariipastukh <nazariy2002n@gmail.com>
2023-10-24 19:13:09 +0300

    hw3.3 joi

--- src/components/Cars/CarForm.js
+++ src/components/Cars/CarForm.js
@@ -3,15 +3,20 @@ import {useForm} from "react-hook-form";
 
 import {carsService} from "../../services";
 import styles from '../../styles/Form.module.css'
+import {carValidator} from "../../validators/carValidator";
+import {joiResolver} from '@hookform/resolvers/joi'
 
 export const CarForm = ({setTrigger, carForUpdate, setCarForUpdate}) => {
-    const {register, reset, handleSubmit, setValue, /*formState: {errors, isValid}*/} = useForm()
+    const {register, reset, handleSubmit, setValue, formState: {errors}} = useForm({
+        resolver: joiResolver(carValidator),
+        mode: "onSubmit",
+    })
 
     useEffect(() => {
         if (carForUpdate) {
-            setValue('brand', carForUpdate.brand)
-            setValue('year', carForUpdate.year)
-            setValue('price', carForUpdate.price)
+            setValue('brand', carForUpdate.brand, {shouldValidate: true})
+            setValue('year', carForUpdate.year, {shouldValidate: true})
+            setValue('price', carForUpdate.price, {shouldValidate: true})
         }
     }, [carForUpdate, setValue])
 
@@ -31,9 +36,18 @@ export const CarForm = ({setTrigger, carForUpdate, setCarForUpdate}) => {
     return (
         <div className={styles.wrapper}>
             <form onSubmit={handleSubmit(carForUpdate ? updateCar : saveCar)}>
-                <input type={'text'} placeholder={'brand'} {...register('brand')}  />
-                <input type={'text'} placeholder={'year'} {...register('year')}  />
-                <input type={'text'} placeholder={'price'} {...register('price')} />
+                <input type={'text'} placeholder={'brand'} {...register('brand')} />
+                {
+                    errors.brand && <div style={{color: 'red'}}> {errors.brand.message}</div>
+                }
+                <input type={'text'} placeholder={'year'} {...register('year', {valueAsNumber: true})} />
+                {
+                    errors.year && <div style={{color:'red'}}> {errors.year.message}</div>
+                }
+                <input type={'text'} placeholder={'price'} {...register('price', {valueAsNumber: true})} />
+                {
+                    errors.price && <div style={{color:'red'}}> {errors.price.message}</div>
+                }
                 <button>{carForUpdate ? 'Update Car' : 'Save Car'}</button>
             </form>
         </div>
