0cb67c0787b3c39a2716c5277ab0687b39146542
nazariipastukh <nazariy2002n@gmail.com>
2023-11-23 16:50:29 +0200

    hw10 fix commit

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,12 +1,19 @@
+import {Component} from "react";
 import {Outlet} from "react-router-dom";
 
 import {Header} from "../components";
 
-export const MainLayout = () => {
-    return (
-        <div>
-            <Header/>
-            <Outlet/>
-        </div>
-    );
-};
+class MainLayout extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Outlet/>
+            </div>
+        );
+    }
+}
+
+export {
+    MainLayout
+}
